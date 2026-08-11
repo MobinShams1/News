c72a74cbb404b087a904f50e4c88fd46b400fd84
Mobin <themobin2003@gmail.com>
2026-08-11 22:18:18 +0330

    fixed a problem about session

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,21 +1,10 @@
-import { cookies } from "next/headers";
-import { prisma } from "@/lib/prisma";
 import AdminLayoutWrapper from "@/components/admin/dashboard/adminLayoutWrapper";
+import { getAuthAdmin } from "@/lib/auth";
 
-async function getAdminUser() {
-  const cookieStore = await cookies();
-  const userId = cookieStore.get("admin_token")?.value;
 
-  if (!userId) return null;
-
-  return await prisma.users.findUnique({
-    where: { id: userId },
-    select: { name: true, phone: true, role: true },
-  });
-}
 
 export default async function AdminLayout({ children }: { children: React.ReactNode }) {
-  const user = await getAdminUser();
-
-  return <AdminLayoutWrapper user={user}>{children}</AdminLayoutWrapper>;
+  const currentadmin = await getAuthAdmin();
+  
+  return <AdminLayoutWrapper user={currentadmin}>{children}</AdminLayoutWrapper>;
 }
